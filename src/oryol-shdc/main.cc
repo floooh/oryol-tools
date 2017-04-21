3d08dc1d73bc40c1ef7def82dd15f16d9a8a4a54
Andre Weissflog <floooh@gmail.com>
2017-04-21 20:05:16 +0200

    Started to work on oryol-shdc tool

--- src/oryol-shdc/main.cc
+++ src/oryol-shdc/main.cc
@@ -1,9 +1,54 @@
 //------------------------------------------------------------------------------
 //  oryol-shdc
 //------------------------------------------------------------------------------
+#include "ExportUtil/CmdLineArgs.h"
+#include "ExportUtil/Log.h"
+#include "spirv_hlsl.hpp"
+#include "spirv_msl.hpp"
 #include <stdio.h>
 
-int main(int argc, const char** args) {
-    printf("Hello World!\n");
+using namespace OryolTools;
+using namespace spv;
+using namespace spirv_cross;
+using namespace std;
+
+static vector<uint32_t> read_spirv_file(const string& path) {
+    FILE* fp = fopen(path.c_str(), "rb");
+    if (!fp) {
+        Log::Fatal("Failed to open SPIRV file '%s'\n", path.c_str());
+    }
+    fseek(fp, 0, SEEK_END);
+    long len = ftell(fp) / sizeof(uint32_t);
+    fseek(fp, 0, SEEK_SET);
+    vector<uint32_t> spirv(len);
+    if (fread(spirv.data(), sizeof(uint32_t), len, fp) != size_t(len)) {
+        Log::Fatal("Error reading SPIRV file '%s'\n", path.c_str());
+    }
+    fclose(fp);
+    return spirv;
+}
+
+int main(int argc, const char** argv) {
+    CmdLineArgs args;
+    args.AddBool("-help", "show help");
+    args.AddString("-spirv", "SPIR-V input file", "");
+    if (!args.Parse(argc, argv)) {
+        Log::Warn("Failed to parse args!\n");
+        return 10; 
+    }
+
+    if (args.HasArg("-help")) {
+        Log::Info("Oryol SPIR-V to GLSL/HLSL/MSL cross-compiler\n"
+                  "Based on SPIRV-Cross: https://github.com/KhronosGroup/SPIRV-Cross\n");
+        args.ShowHelp();
+        return 0;                  
+    }
+    string spirvPath = args.GetString("-spirv");
+    if (spirvPath.empty()) {
+        Log::Fatal("-spirv arg expected");
+    }
+    auto spirv = read_spirv_file(spirvPath);
+    auto glsl = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
     return 0;
 }
