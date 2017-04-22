191a336888eb98ccb6ba010b59107198108b3450
Andre Weissflog <floooh@gmail.com>
2017-04-22 13:19:51 +0200

    Add pystring dep and started with separate shader source outputs

--- src/oryol-shdc/main.cc
+++ src/oryol-shdc/main.cc
@@ -5,6 +5,7 @@
 #include "ExportUtil/Log.h"
 #include "spirv_hlsl.hpp"
 #include "spirv_msl.hpp"
+#include "pystring.h"
 #include <stdio.h>
 
 using namespace OryolTools;
@@ -12,6 +13,7 @@ using namespace spv;
 using namespace spirv_cross;
 using namespace std;
 
+//------------------------------------------------------------------------------
 static vector<uint32_t> read_spirv_file(const string& path) {
     FILE* fp = fopen(path.c_str(), "rb");
     if (!fp) {
@@ -28,6 +30,49 @@ static vector<uint32_t> read_spirv_file(const string& path) {
     return spirv;
 }
 
+//------------------------------------------------------------------------------
+void to_glsl_100(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".glsl100.glsl";
+    auto compiler = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
+void to_glsl_120(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".glsl120.glsl";
+    auto compiler = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
+void to_glsl_es3(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".glsles3.glsl";
+    auto compiler = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
+void to_glsl_330(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".glsl330.glsl";
+    auto compiler = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
+void to_hlsl_sm5(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".hlsl";
+    auto compiler = unique_ptr<CompilerHLSL>(new CompilerHLSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
+void to_mlsl(const vector<uint32_t>& spirv, const string& base_path) {
+    string dst_path = base_path + ".metal";
+    auto compiler = unique_ptr<CompilerMSL>(new CompilerMSL(spirv));
+
+}
+
+//------------------------------------------------------------------------------
 int main(int argc, const char** argv) {
     CmdLineArgs args;
     args.AddBool("-help", "show help");
@@ -43,12 +88,23 @@ int main(int argc, const char** argv) {
         args.ShowHelp();
         return 0;                  
     }
-    string spirvPath = args.GetString("-spirv");
-    if (spirvPath.empty()) {
+    string spirv_path = args.GetString("-spirv");
+    if (spirv_path.empty()) {
         Log::Fatal("-spirv arg expected");
     }
-    auto spirv = read_spirv_file(spirvPath);
-    auto glsl = unique_ptr<CompilerGLSL>(new CompilerGLSL(spirv));
+
+    // load SPIRV byte code
+    auto spirv = read_spirv_file(spirv_path);
+
+    // ...translate and write to output files...
+    string base_path, ext;
+    pystring::os::path::splitext(base_path, ext, spirv_path);
+    to_glsl_100(spirv, base_path);
+    to_glsl_120(spirv, base_path);
+    to_glsl_es3(spirv, base_path);
+    to_glsl_330(spirv, base_path);
+    to_hlsl_sm5(spirv, base_path);
+    to_mlsl(spirv, base_path);
 
     return 0;
 }
